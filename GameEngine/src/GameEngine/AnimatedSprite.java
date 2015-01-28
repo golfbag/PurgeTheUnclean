c0a6d88bd259396953f938c585ae178db2037f6b
Carl Stjernfeldt <carl.elgestad@gmail.com>
2015-01-28 13:58:55 +0100

    Lade till countFrame och countRow

--- GameEngine/src/GameEngine/AnimatedSprite.java
+++ GameEngine/src/GameEngine/AnimatedSprite.java
@@ -1,9 +1,19 @@
 package GameEngine;
 
 public abstract class AnimatedSprite extends Sprite {
+	private int countFrame;
+	private int countRow;
 
 	public AnimatedSprite(int xPos, int yPos, String fileName) {
 		super(xPos, yPos, fileName);
+	}
+	
+	public void setFrame(int frame){
+		this.countFrame = frame;
+	}
+	
+	public void setRow(int row){
+		this.countRow = row;
 	}
 
 }
