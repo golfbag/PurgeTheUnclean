571f442e2cac9ff3dadf4ab163d72f450a542155
Carl Stjernfeldt <carl.elgestad@gmail.com>
2015-01-28 16:49:37 +0100

    Test

--- GameEngine/src/GameEngine/SoundEngine.java
+++ GameEngine/src/GameEngine/SoundEngine.java
@@ -1,6 +1,13 @@
 package GameEngine;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
+//Static class
 public final class SoundEngine {
+	
+	private SoundEngine(){}
 	
 	public static void playSound(String name){
 		Loader.getSound(name).play();
