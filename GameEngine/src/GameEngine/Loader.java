d20ae5ddfb2f469aa68bce06b8103097442113c6
Carl Stjernfeldt <carl.elgestad@gmail.com>
2015-01-28 14:33:30 +0100

    Ändrade i Loader-klassen

--- GameEngine/src/GameEngine/Loader.java
+++ GameEngine/src/GameEngine/Loader.java
@@ -3,18 +3,17 @@ package GameEngine;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.imageio.ImageIO;
 
 public class Loader {
-	public BufferedImage loadImage(String filePath){
+	private HashMap<String, BufferedImage> imageMap = new HashMap<>();
+	
+	public void loadImage(String name, String filePath){
 		try {
-		    return ImageIO.read(new File(filePath));
+		    imageMap.put(name, ImageIO.read(new File(filePath)));
 		} catch (IOException e) {
-			return null;
 		}
 	}
-	
-//	public String loadSound(String filePath){
-//		
-//	}
 }
