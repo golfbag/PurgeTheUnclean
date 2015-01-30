bd9b541745feb4ae168e8980a10b68836c5b2ed7
Carl Stjernfeldt <carl.elgestad@gmail.com>
2015-01-30 14:08:00 +0100

    Lade till mappen "Sounds"

--- GameEngine/src/TestGame/TheGame.java
+++ GameEngine/src/TestGame/TheGame.java
@@ -10,13 +10,12 @@ public class TheGame {
 	public static void main(String[] args) {
 		GameWindow game = new GameWindow("Test", 640, 480);
 		try {
-			Loader.loadSound("helicopter",  new URL("file:///C:/Users/Administrator/Downloads/soundscrate-helicopter.wav"));
-			Loader.loadSound("rocket",  new URL("file:///C:/Users/Administrator/Downloads/soundscrate-rocket.wav"));
+			Loader.loadSound("helicopter",  new URL("file:///C:/Users/Administrator/Documents/PurgeTheUnclean/GameEngine/src/TestGame/Sounds/soundscrate-helicopter.wav"));
+			Loader.loadSound("rocket",  new URL("file:///C:/Users/Administrator/Documents/PurgeTheUnclean/GameEngine/src/TestGame/Sounds/soundscrate-rocket.wav"));
 			SoundEngine.loopSound("helicopter");
 			SoundEngine.playSound("rocket");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
