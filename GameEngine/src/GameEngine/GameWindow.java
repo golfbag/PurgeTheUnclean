439088cbaaf37084e779dd840813a76b11c02a27
Carl Stjernfeldt <carl.elgestad@gmail.com>
2015-01-30 13:28:58 +0100

    Fixade i GameWindow och lade till ett testfönster

--- GameEngine/src/GameEngine/GameWindow.java
+++ GameEngine/src/GameEngine/GameWindow.java
@@ -3,7 +3,7 @@ package GameEngine;
 import javax.swing.JFrame;
 
 public class GameWindow extends JFrame {
-	GameWindow(String windowName, int width, int height){
+	public GameWindow(String windowName, int width, int height){
 		super(windowName);
 		setSize(width, height);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
--- GameEngine/src/TestGame/TheGame.java
+++ GameEngine/src/TestGame/TheGame.java
@@ -1,10 +1,22 @@
 package TestGame;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import GameEngine.*;
+
 public class TheGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameWindow game = new GameWindow("Test", 640, 480);
+		String url = "file:///C:/Users/Administrator/Desktop/Kalimba.mp3";
+		try {
+			Loader.loadSound("test",  new URL(url));
+			SoundEngine.playSound("test");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
